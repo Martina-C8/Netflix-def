4fbde6665e3f26d04f8e83332918e99f63b3ef8b
Martina-C8 <casettamartina@outlook.it>
2024-05-09 09:37:29 +0200

    js part

--- script.js
+++ script.js
@@ -20,3 +20,23 @@ $(document).ready(function(){
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
   }
+
+
+  window.addEventListener('scroll', function () {
+    var contentToShow = document.getElementById('contentToShow');
+    var contentToHide = document.getElementById('contentToHide');
+    var scrollPosition = window.scrollY;
+
+    // Altezza della finestra del browser
+    var windowHeight = window.innerHeight;
+
+    // Posizione dell'elemento da mostrare rispetto all'inizio della pagina
+    var contentToShowPosition = contentToShow.getBoundingClientRect().top;
+
+    // Se l'elemento da mostrare è visibile nella finestra del browser, mostra il contenuto nascosto
+    if (contentToShowPosition < windowHeight) {
+        contentToHide.style.display = 'block';
+    } else {
+        contentToHide.style.display = 'none';
+    }
+});
