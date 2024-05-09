0573f1565446c6a14100305f038b13bef1dc2420
Martina-C8 <casettamartina@outlook.it>
2024-05-09 09:28:34 +0200

    modifiche

--- script.js
+++ script.js
@@ -2,3 +2,21 @@ $(document).ready(function(){
     $('.carousel').carousel();
   });
   
+
+
+
+  window.onscroll = function () { scrollFunction() };
+
+  function scrollFunction() {
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+          document.getElementById("scrollToTop").style.display = "block";
+      } else {
+          document.getElementById("scrollToTop").style.display = "none";
+      }
+  }
+
+
+  function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
